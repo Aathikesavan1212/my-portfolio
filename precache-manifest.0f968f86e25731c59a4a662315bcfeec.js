ae3cac06575f7b50f556c76d62e42c367507ded2
Aathi Kesavan <aathikesavan122@gmail.com>
2025-09-03 11:01:05 +0530

    Updates

---
+++ precache-manifest.0f968f86e25731c59a4a662315bcfeec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4355477c5c5d996ac7323badb3e2b7a5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "c0b4430c311e965a1ff6",
+    "url": "/my-portfolio/static/css/2.83d756e6.chunk.css"
+  },
+  {
+    "revision": "2ed42596a1a147a3a9f9",
+    "url": "/my-portfolio/static/css/main.3695e886.chunk.css"
+  },
+  {
+    "revision": "c0b4430c311e965a1ff6",
+    "url": "/my-portfolio/static/js/2.c1d86837.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/my-portfolio/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "2ed42596a1a147a3a9f9",
+    "url": "/my-portfolio/static/js/main.24137198.chunk.js"
+  },
+  {
+    "revision": "6605eb620141aa5f062e",
+    "url": "/my-portfolio/static/js/runtime~main.e00bd932.js"
+  }
+]);
